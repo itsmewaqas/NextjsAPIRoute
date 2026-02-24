aecb7542dc2353ec3c633ff0a700f6e4c2691c13
m.waqas <m.waqas@avanzasolutions.com>
2026-02-24 13:37:56 +0500

    third intial commit

--- src/app/Services/Remove.jsx
+++ src/app/Services/Remove.jsx
@@ -1,5 +1,6 @@
 "use client"
 import { useRouter } from 'next/navigation';
+import { API_BASE_URL } from '../../../config/constant';
 
 export default function Remove(props) {
     const userID = props.id;
@@ -8,7 +9,7 @@ export default function Remove(props) {
     const router = useRouter();
 
     const deleteUser = async () => {
-        let result = await fetch(`/API/List/${userID}`, {
+        let result = await fetch(`${API_BASE_URL}/API/List/${userID}`, {
             method: "Delete",
         });
         let response = await result.json();
