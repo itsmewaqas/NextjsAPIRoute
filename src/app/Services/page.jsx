58b7869f586f342537b62cb55c3b589574c043ca
m.waqas <m.waqas@avanzasolutions.com>
2026-02-24 14:52:50 +0500

    fifth intial commit

--- src/app/Services/page.jsx
+++ src/app/Services/page.jsx
@@ -1,25 +1,84 @@
+// import Link from "next/link";
+// import Remove from "./Remove";
+// import { API_BASE_URL } from "../../../config/constant";
+
+// async function GetAPIData() {
+//     let data = await fetch(`${API_BASE_URL}/API/List`);
+//     let result = await data.json();
+//     return result;
+// }
+
+// export const dynamic = "force-dynamic";
+
+// export default async function Page() {
+
+//     const GetUser = await GetAPIData();
+//     console.log(GetUser, 'GetUser')
+
+//     return (
+//         <div>
+//             <h1>Services Page</h1>
+//             <ul>
+
+//                 {GetUser.map((item) => (
+//                     <li key={item.id}>
+//                         <p>ID : {item.id}</p>
+//                         <p>Name : {item.name}</p>
+//                         <p>Email : {item.email}</p>
+//                         <p>Cell : {item.cell}</p>
+//                         <p>Age : {item.age}</p>
+//                         <Link href={`Services/${item.id}`}>View</Link>
+//                         <br />
+//                         <Link href={`Services/${item.id}/update`}>Edit</Link>
+//                         <br />
+//                         <Remove id={item.id} />
+//                         <br />
+//                         <br />
+//                     </li>
+//                 ))}
+
+//             </ul>
+//         </div>
+//     );
+// }
+
+
+// export function generateMetadata() {
+//     return {
+//         title: 'Services Page Title',
+//         description: "Services Page Description",
+//     };
+// }
+
+
+
 import Link from "next/link";
 import Remove from "./Remove";
 import { API_BASE_URL } from "../../../config/constant";
 
+export const dynamic = "force-dynamic";
+
 async function GetAPIData() {
-    let data = await fetch(`${API_BASE_URL}/API/List`);
+    let data = await fetch(`${API_BASE_URL}/api/List`, {
+        cache: "no-store"
+    });
+
+    if (!data.ok) {
+        throw new Error("Failed to fetch data");
+    }
+
     let result = await data.json();
     return result;
 }
 
-export const dynamic = "force-dynamic";
-
 export default async function Page() {
 
     const GetUser = await GetAPIData();
-    console.log(GetUser, 'GetUser')
 
     return (
         <div>
             <h1>Services Page</h1>
             <ul>
-
                 {GetUser.map((item) => (
                     <li key={item.id}>
                         <p>ID : {item.id}</p>
@@ -27,21 +86,19 @@ export default async function Page() {
                         <p>Email : {item.email}</p>
                         <p>Cell : {item.cell}</p>
                         <p>Age : {item.age}</p>
-                        <Link href={`Services/${item.id}`}>View</Link>
+
+                        <Link href={`/Services/${item.id}`}>View</Link>
                         <br />
-                        <Link href={`Services/${item.id}/update`}>Edit</Link>
+                        <Link href={`/Services/${item.id}/update`}>Edit</Link>
                         <br />
                         <Remove id={item.id} />
-                        <br />
-                        <br />
+                        <br /><br />
                     </li>
                 ))}
-
             </ul>
         </div>
     );
 }
-
 
 export function generateMetadata() {
     return {
