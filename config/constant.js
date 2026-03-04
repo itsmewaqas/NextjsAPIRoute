4d01efa6a864c99757d4d8326619f3ee21ae65b8
m.waqas <m.waqas@avanzasolutions.com>
2026-03-04 12:40:47 +0500

    tenth intial commit

--- config/constant.js
+++ config/constant.js
@@ -1,4 +1,3 @@
-//export const API_BAS_URL = process.env.NODE_ENV == "development" ? "http://localhost:3000" : "https://nextjs-api-route-dun.vercel.app";
 
-export const API_BASE_URL = process.env.API_BASE_URL;
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
